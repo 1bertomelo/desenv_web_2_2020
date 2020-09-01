f0d63020f984ccbffb881420b34e0088a6651fe7
1bertomelo <1bertomelo@gmail.com>
2020-09-01 15:34:43 -0300

    primeira versão

---
+++ códigos aula/GestaoDeProduto/GestaoDeProduto/GestaoDeProduto/Controllers/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestaoDeProduto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeProduto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get( ){ return Ok("Listagem de categoria"); }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) { return Ok("Detalhes da categoria"); }
+        [HttpPost]
+        public async Task<IActionResult> Post(Categoria categoria) { return Ok("Categoria criada com sucesso"); }
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, Categoria categoria) { return Ok("Categoria atualizada com sucesso"); }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) { return Ok("Categoria removida com sucesso!"); }
+
+    }
+}
