b6252620ac0f6193d4b4f663f96ce26c614076e4
1bertomelo <1bertomelo@gmail.com>
2020-09-16 20:24:20 -0300

    exercicio lita 1

---
+++ códigos aula/Covid19/Codiv19.API/Codiv19.API/Controllers/PacienteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Codiv19.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codiv19.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Litagem de todos os pacientes");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("dados do paciente id");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Paciente paciente)
+        {
+            return Ok("Paciente cadastrado com sucesso");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Paciente paciente)
+        {
+            return Ok("Paciente atualizado com sucesso");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int  id)
+        {
+            return Ok("Paciente deleteado com sucesso");
+        }
+
+    }
+}
